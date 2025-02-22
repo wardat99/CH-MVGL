d8aa80c4488ef388f254cb20e6c84ba371e49442
Mohammad Al-Wardat <60725003+wardat99@users.noreply.github.com>
2025-02-22 02:39:33 -0500

    Add files via upload

--- functions/get_hub_graph.m
+++ functions/get_hub_graph.m
@@ -4,7 +4,7 @@ function [Adj,A,hn] = get_hub_graph(n,num_nodes,num_views,p)
 rng('shuffle');
 hn=[];
 x = setdiff(1:n,hn);
-percentage_rng=[0.7 0.71 0.72 0.73 0.74 0.75];
+percentage_rng=0.88:0.2:1;
 percentage=percentage_rng(randi(numel(percentage_rng)));
 for k=1:num_views
     Adj(:,:,k) = create_ER_Graph(n,p);
@@ -26,8 +26,8 @@ end
 
 
     function vector=get_vector(n,percentage)
-    num_ones = round(percentage * n); % number of 1's (80% of n)
-    num_zeros = n - num_ones; % number of 0's (20% of n)
+    num_ones = round(percentage * n); % number of 1's 
+    num_zeros = n - num_ones; % number of 0's 
     vector = [ones(1, num_ones), zeros(1, num_zeros)];
     vector = vector(randperm(n));
     end
